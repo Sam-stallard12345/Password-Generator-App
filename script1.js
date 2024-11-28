fd3bce0406f6cbf2c548635a6b5c8a5900a2fd5b
Sam Stallard <5n5j73bh@students.codeinstitute.net>
2024-11-28 09:07:24 +0000

    added pseudo JS

--- script1.js
+++ script1.js
@@ -1,1 +1,27 @@
-const 
+// Questions and Answers
+
+const questions = [ {
+    question:   
+}
+
+// Get DOM elements
+const questionDiv = document.querySelectorAll('.question');
+const answerDiv = document.querySelectorAll('.answer');
+
+// Add event listener to each answer
+
+// Check Answer on button submit
+const submitButton = document.querySelector('.submit');
+const result = document.querySelector('.result');
+
+submitButton.addEventListener('click', () => {
+    let score = 0;
+    answers.forEach((answer, index) => {
+        if (answer.checked) {
+            if (answer.value === 'correct') {
+                score++;
+            }
+        }
+    });
+    result.textContent = `Your score is ${score}`;
+}
