cc496c89a806bb57954e8e8248a573e9b17d49bb
Rayyan Farani <7a6z2prx@students.codeinstitute.net>
2024-11-28 15:28:02 +0000

    added length formula and password generation formula

--- PasswordGen-Page/assets /js/script.js
+++ PasswordGen-Page/assets /js/script.js
@@ -14,8 +14,25 @@ function generate() {
         dictionary += "!@£$%^&*(){}[];:<>";
     }
 
-    // LENGHTH OF THE CHARACATERS 
+    // LENGTH OF THE CHARACATERS 
      
     const length = document.querySelector(
-        'input[type="range"]').value;
+        'input[type="range"]').value;   
+
+    
+    
+    // ALERT IF NO DICTIONARY SELECTED OR RANGE IS TOO SHORT
+
+    if (length < 1 || dictionary.length === 0) {
+        alert("Range is too short or no dictionary selected");
+    }
+
+
+    // PASSWORD GENERATOR
+
+    let password ="";
+    for (let i = 0 ; i < length ; i++) {
+        const index = Math.floor(Math.random() * dictionary.length)
+        password += dictionary[index];
+    }
 }
