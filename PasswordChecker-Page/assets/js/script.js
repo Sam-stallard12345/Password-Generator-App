afe2e5a707d6f7589309de18a6529a2922def2bf
Rayyan Farani <7a6z2prx@students.codeinstitute.net>
2024-12-01 23:05:19 +0000

    added js for password check functionality

---
+++ PasswordChecker-Page/assets/js/script.js
@@ -0,0 +1,25 @@
+let password = document.getElementById("password"); 
+let power = document.getElementById("powerbar");
+
+function checkPassword(){
+    let point = 0;
+    let value = password.value;
+    let widthPower = ["1%", "25%", "50%", "75%", "100%"];
+    let colorPower = ["red", "orange", "yellow", "green", "blue"];
+
+    // use if statement check if password length is greater than 6
+
+    if (value.length >= 6){
+        let arrayTest = [/[a-z]/, /[A-Z]/, /[0-9]/, /[$@#]/];
+
+        arrayTest.forEach((item) => {
+            if (item.test(value)) {
+                point += 1;
+            }
+        });
+    }
+    power.style.width = widthPower[point];
+    power.style.backgroundColor = colorPower[point];
+};
+
+checkPassword();    
