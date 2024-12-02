41e271beedd3d9e6ec38f6e8ad36d0b27403894d
Rayyan Farani <7a6z2prx@students.codeinstitute.net>
2024-12-02 09:53:40 +0000

    Added hero

--- PasswordChecker-Page/assets/js/script.js
+++ PasswordChecker-Page/assets/js/script.js
@@ -1,25 +1,27 @@
-let password = document.getElementById("password"); 
-let power = document.getElementById("powerbar");
 
-password.oninput = function (){
-    let point = 0;
-    let value = password.value;
-    let widthPower = ["1%", "25%", "50%", "75%", "100%"];
-    let colorPower = ["red", "orange", "yellow", "green", "blue"];
+// script.js
 
-    // use if statement check if password length is greater than 6
+document.addEventListener("DOMContentLoaded", function() {
+    let password = document.getElementById("password");
+    let power = document.getElementById("power-point");
+    password.oninput = function () {
+        let point = 0;
+        let value = password.value;
+        let widthPower = 
+            ["1%", "25%", "50%", "75%", "100%"];
+        let colorPower = 
+            ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"];
 
-    if (value.length >= 6){
-        let arrayTest = [/[a-z]/, /[A-Z]/, /[0-9]/, /[$@#]/];
-
-        arrayTest.forEach((item) => {
-            if (item.test(value)) {
-                point += 1;
-            }
-        });
-    }
-    power.style.width = widthPower[point];
-    power.style.backgroundColor = colorPower[point];
-};
-
-checkPassword();    
+        if (value.length >= 6) {
+            let arrayTest = 
+                [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
+            arrayTest.forEach((item) => {
+                if (item.test(value)) {
+                    point += 1;
+                }
+            });
+        }
+        power.style.width = widthPower[point];
+        power.style.backgroundColor = colorPower[point];
+    };
+});
